219f3640aa30bd5c50197aa369847343747f260d
indranilgupta <indranilgupta@gmail.com>
2021-01-07 00:53:48 +0530

    Create pythonRefresher.py

---
+++ pythonRefresher.py
@@ -0,0 +1,4 @@
+sizeInput = input("How big is your house (in square feet): ")
+squareFeet = int(sizeInput)
+squareMeters = squareFeet / 10.8
+print (f"{squareFeet} square feet is {squareMeters:.2f} square meters")
